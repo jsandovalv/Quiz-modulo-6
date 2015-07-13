6b88f99aa94ed82aaf49315006b010188ce144f7
jorge sandoval <sandoval.jorge.v@gmail.com>
2015-07-13 16:10:36 -0400

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,14 +1,33 @@
 var path = require("path");
 
+//Postgres DATABASE_URL = postgre://user:passwd@host:port/database
+//SQLite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 //Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite:
-var sequelize = new Sequelize(null,null,null,
-              { dialect: "sqlite" , store: "quiz.sqlite"}
-            );
+// Usar DDBB SQLite o Postgres
+var sequelize = new Sequelize(DB_name,user,pwd,
+              { dialect: protocol ,
+                protocol: protocol,
+                port: port,
+                host: host,
+                storage: storage,
+                omitNull: true
+        }
+    );
 
 // Importar la definicion de la tabla Quiz en quiz.js
+var quiz_path= path.join(__dirname,'quiz');
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
 //exportar definicion de tabla Quiz
@@ -16,8 +35,9 @@ exports.Quiz = Quiz;
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().success(function(){
+  //success() ejecuta el manejador una vez creada la tabla
   Quiz.count().success(function(count){
-    if(count ===0 ) {
+    if(count ===0 ) { //la tabla se inicializa solo si esta vacia
       Quiz.create({ pregunta: 'Capital de Italia',
                     respuesta: 'Roma'
                   })
