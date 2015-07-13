82c10e4b0e9b5410afb0f7f7dc0485eea527b380
jorge sandoval <sandoval.jorge.v@gmail.com>
2015-07-13 15:19:56 -0400

    sequelize.js y SQLite

---
+++ models/quiz.js
@@ -0,0 +1,8 @@
+// Definicion del modelo Quiz
+
+module.exports = function(sequelize, DataType){
+  return sequelize.define('Quiz',
+              { pregunta: DataType.STRING,
+                respuesta: DataType.STRING,
+              });
+}
