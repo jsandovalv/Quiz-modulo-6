2458eb02235b8d81888f98936eebe055e7047049
jorge sandoval <sandoval.jorge.v@gmail.com>
2015-07-13 22:45:19 -0400

    Multiples preguntas

--- models/models.js
+++ models/models.js
@@ -36,12 +36,15 @@ exports.Quiz = Quiz;
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().success(function(){
   //success() ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function(count){
+  Quiz.count().then(function(count){
     if(count ===0 ) { //la tabla se inicializa solo si esta vacia
       Quiz.create({ pregunta: 'Capital de Italia',
                     respuesta: 'Roma'
+                  });
+      Quiz.create({ pregunta: 'Capital de Portugal',
+                    respuesta: 'Lisboa'
                   })
-      .success(function() { console.log('Base de datos inicializada')});
+      .then(function() { console.log('Base de datos inicializada')});
     };
   });
 });
