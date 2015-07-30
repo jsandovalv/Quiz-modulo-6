91c7a8cc3e9d5fa540a5fe9b8a4cb7ee10c71155
jorge sandoval <sandoval.jorge.v@gmail.com>
2015-07-29 21:56:30 -0400

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId' , quizController.load); //autoload :quizId
+router.param('commentId' , commentController.load); //autoload :commentId
 
 //Definicion de rutas de sesion
 router.get('/login',sessionController.new); //formulario login
@@ -23,13 +24,15 @@ router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new_pregunta' , quizController.new_pregunta);
-router.post('/quizes/create' , quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.post('/quizes/create' , sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired , quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired , quizController.update);
+router.delete('/quizes/:quizId(\\d+)' , sessionController.loginRequired , quizController.destroy);
 
+//Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', commentController.publish);
 
 router.get('/author',function(req, res) {
   res.render('author',{autor: "Jorge Sandoval", pais: "Chile",errors: []});
